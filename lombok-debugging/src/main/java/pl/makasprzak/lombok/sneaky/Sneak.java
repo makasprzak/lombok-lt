33459bad96331ee3f3722799df7fb71d367ab687
Maciej Kasprzak <mkasprzak@pgs-soft.com>
2017-07-11 20:19:54 +0200

    credits

--- lombok-debugging/src/main/java/pl/makasprzak/lombok/sneaky/Sneak.java
+++ lombok-debugging/src/main/java/pl/makasprzak/lombok/sneaky/Sneak.java
@@ -1,5 +1,9 @@
 package pl.makasprzak.lombok.sneaky;
 
+/**
+ * http://www.mail-archive.com/javaposse@googlegroups.com/msg05984.html
+ * http://www.philandstuff.com/2012/04/28/sneakily-throwing-checked-exceptions.html
+ */
 public class Sneak {
     public static void sneakyThrow(Throwable t) {
         if ( t == null ) throw new NullPointerException("t");
